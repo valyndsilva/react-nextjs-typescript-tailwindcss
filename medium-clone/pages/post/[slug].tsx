baf774ee70a6364b827deb9f444a8d314675caf8
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-06-01 15:33:12 +0100

    Incremental Static Generation, SSR, PortableText

--- medium-clone/pages/post/[slug].tsx
+++ medium-clone/pages/post/[slug].tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import { sanityClient, urlFor } from '../../lib/sanity';
 import { Post } from '../../typings';
-
+import PortableText from 'react-portable-text';
 interface Props {
   post: Post;
 }
@@ -13,6 +13,57 @@ function Post({ post }: Props) {
   return (
     <main>
       <Header />
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+        <div className="flex  items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+          />
+          <p className="font-extralight text-sm">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div>
+          {/* <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+          /> */}
+          <PortableText
+            className="mt-10"
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: (children: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
     </main>
   );
 }
@@ -77,5 +128,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       post,
     },
     revalidate: 60, // this enables ISR and updates the old cache version after 60 seconds
+    // It basically server side renders the page after 60 seconds and caches it and that one gets served for the next 60 secs in a static way
   };
 };
