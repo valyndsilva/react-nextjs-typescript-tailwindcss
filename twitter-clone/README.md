383a0d1b4725043de0afdbe358a7fd6cd5832c46
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-06-03 16:26:11 +0100

    Implement Image URL Functionalit on TweetBox

--- twitter-clone/README.md
+++ twitter-clone/README.md
@@ -453,3 +453,73 @@ export default TweetBox;
 
 
 ```
+
+## Image URL Functionality for TweetBox.tsx:
+
+```
+  const [image, setImage] = useState<string>('');
+  const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    // if no image assigned  don't attach an image
+    if (!imageInputRef.current?.value) return;
+    if (imageInputRef.current) {
+      setImage(imageInputRef.current.value);
+      imageInputRef.current.value = ''; //clear the input
+      setImageUrlBoxIsOpen(false);
+    }
+  };
+
+
+return (
+...
+<PhotographIcon
+onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
+className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"
+/>
+...
+           <button
+              disabled={!input || !session}
+              className="bg-twitter rounded-full px-5 py-2 font-bold text-white disabled:opacity-40"
+            >
+              Tweet
+            </button>
+          </div>
+          {imageUrlBoxIsOpen && (
+            <form className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4">
+              <input
+                ref={imageInputRef}
+                className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white"
+                type="text"
+                placeholder="Enter Image URL..."
+              />
+              <button
+                type="submit"
+                onClick={addImageToTweet}
+                className="font-bold text-white"
+              >
+                Add Image
+              </button>
+            </form>
+          )}
+          {image && (
+            <img
+              src={image}
+              className="mt-10 h-40 w-full rounded-xl object-contain shadow-lg"
+              alt=""
+            />
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default TweetBox;
+
+```
--- twitter-clone/components/TweetBox.tsx
+++ twitter-clone/components/TweetBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   CalendarIcon,
   EmojiHappyIcon,
@@ -10,12 +10,29 @@ import {
 import { useSession } from 'next-auth/react';
 
 function TweetBox() {
+  const { data: session } = useSession();
+  // console.log(session);
+  // console.log(session?.user?.image);
+
   // Keep track if  the user entered something in the tweetbox:
   const [input, setInput] = useState<string>('');
+  const [image, setImage] = useState<string>('');
+  const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
 
-  const { data: session } = useSession();
-  console.log(session);
-  // console.log(session?.user?.image);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    // if no image assigned  don't attach an image
+    if (!imageInputRef.current?.value) return;
+    if (imageInputRef.current) {
+      setImage(imageInputRef.current.value);
+      imageInputRef.current.value = ''; //clear the input
+      setImageUrlBoxIsOpen(false);
+    }
+  };
+
   return (
     <div className="flex space-x-2 p-5">
       <img
@@ -34,7 +51,10 @@ function TweetBox() {
           />
           <div className="flex items-center">
             <div className="flex flex-1 space-x-2 text-twitter">
-              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <PhotographIcon
+                onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
+                className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"
+              />
               <SearchCircleIcon className="h-5 w-5" />
               <ChartBarIcon className="h-5 w-5 rotate-90" />
               <EmojiHappyIcon className="h-5 w-5" />
@@ -48,6 +68,30 @@ function TweetBox() {
               Tweet
             </button>
           </div>
+          {imageUrlBoxIsOpen && (
+            <form className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4">
+              <input
+                ref={imageInputRef}
+                className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white"
+                type="text"
+                placeholder="Enter Image URL..."
+              />
+              <button
+                type="submit"
+                onClick={addImageToTweet}
+                className="font-bold text-white"
+              >
+                Add Image
+              </button>
+            </form>
+          )}
+          {image && (
+            <img
+              src={image}
+              className="mt-10 h-40 w-full rounded-xl object-contain shadow-lg"
+              alt=""
+            />
+          )}
         </form>
       </div>
     </div>
