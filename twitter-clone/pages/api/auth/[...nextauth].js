a38ae790fdae2112e224c39ea522c1da3ef2a97e
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-06-06 12:26:51 +0100

    test 1

--- twitter-clone/pages/api/auth/[...nextauth].js
+++ twitter-clone/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
-import GithubProvider from "next-auth/providers/github";
+// import GithubProvider from "next-auth/providers/github";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -14,10 +14,10 @@ export default NextAuth({
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: "2.0", //opt-in to Twitter OAuth 2.0
     }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_CLIENT_ID,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    // }),
 
     // ...add more providers here
   ],
