fc9ed58d89ff921f665a9211ceb02aae52f3c2d0
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-06-06 13:10:29 +0100

    next.config.js sanity-img-url

--- twitter-clone/next.config.js
+++ twitter-clone/next.config.js
@@ -4,7 +4,8 @@ module.exports = {
   // target: "serverless",
   images: {
     domains: [
-      "*",
+      "cdn.sanity.io",
+      "<http://cd.sanity.io|cd.sanity.io>",
       // "localhost",
       // "lh3.googleusercontent.com",
       // "github.com",
