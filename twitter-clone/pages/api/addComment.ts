21b0300d8701a1c63e944c049bf72b8b575dad33
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-06-07 10:51:39 +0100

    addComment update

--- twitter-clone/pages/api/addComment.ts
+++ twitter-clone/pages/api/addComment.ts
@@ -9,41 +9,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  try {
-    const data: CommentBody = JSON.parse(req.body);
-    console.log(data.tweetId);
+  const data: CommentBody = JSON.parse(req.body);
+  console.log(data.tweetId);
 
-    // mutations sends instruction to the backend and tells the backend how to update the data
-    // https://www.sanity.io/docs/http-mutations
-    const mutations = {
-      mutations: [
-        {
-          create: {
-            _type: "comment",
-            comment: data.comment,
-            username: data.username,
-            profileImage: data.profileImage,
-            tweet: {
-              _type: "reference",
-              _ref: data.tweetId,
-            },
+  // mutations sends instruction to the backend and tells the backend how to update the data
+  // https://www.sanity.io/docs/http-mutations
+  const mutations = {
+    mutations: [
+      {
+        create: {
+          _type: "comment",
+          comment: data.comment,
+          username: data.username,
+          profileImage: data.profileImage,
+          tweet: {
+            _type: "reference",
+            _ref: data.tweetId,
           },
         },
-      ],
-    };
-    const apiEndpoint = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v1/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`;
-    const result = await fetch(apiEndpoint, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.SANITY_API_TOKEN}`,
       },
-      body: JSON.stringify(mutations),
-      method: "POST",
-    });
-    const json = await result.json();
-    res.status(200).json({ message: "Comment Added" });
-  } catch (err) {
-    // 👇️ This runs
-    console.log("Error: ", err as Error);
-  }
+    ],
+  };
+  const apiEndpoint = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v1/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`;
+  const result = await fetch(apiEndpoint, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.SANITY_API_TOKEN}`,
+    },
+    body: JSON.stringify(mutations),
+    method: "POST",
+  });
+  const json = await result.json();
+  res.status(200).json({ message: "Comment Added" });
 }
